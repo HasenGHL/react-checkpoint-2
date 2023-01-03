ea041cd8fe670ebb127029f38b6e908f91f2aa3f
HasenGHL <113119718+HasenGHL@users.noreply.github.com>
2023-01-03 15:34:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//task 1
+
 function dotProduct(v1, v2) {
 
   let dotProduct = 0;
@@ -9,4 +11,20 @@ function dotProduct(v1, v2) {
 
 function areOrthogonal(v1, v2) {
   return dotProduct(v1, v2) === 0;
+}
+
+
+//task 2
+
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let current = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > current) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = current;
+  }
+  return arr;
 }
